2d3b3355768658c4e65c6209ebb1daeb16b51aa3
jefQuery <jeffrey.milberger@gmail.com>
2017-02-13 12:23:43 -0800

    Not the bees! Pass all tests

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,13 @@
 var Grub = function() {
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
 };
 
+Grub.prototype.eat = function(food) {
+  if(this.food === food) { 
+    return "Yum!";
+  } else {
+    return "Yuck!"
+  }
+};
